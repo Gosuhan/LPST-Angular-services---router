ba5e75a85c46f66166b68586051a8f0927c47076
Gosuhan <gosuhan@hotmail.fr>
2018-01-26 14:03:08 +0100

    Foncionnement Menu avec RouterModule OK

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ExperiencesComponent } from './experiences/experiences.component';
+import { ContactComponent } from './contact/contact.component';
+import { TodolistComponent } from './todolist/todolist.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'experiences', component: ExperiencesComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'todolist', component: TodolistComponent },
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
